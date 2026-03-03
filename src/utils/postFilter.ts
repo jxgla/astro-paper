2aad39d704d5e60b3ad4d87e8f92462c19e9668c
jxgla <hjxsdfz@gmail.com>
2026-03-03 10:56:15 +0000

    fix: 移除无效Edit链接，重构双语日期组件，屏蔽示例文章

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -1,11 +1,15 @@
 import type { CollectionEntry } from "astro:content";
 import { SITE } from "@/config";
 
-const postFilter = ({ data }: CollectionEntry<"blog">) => {
+const postFilter = ({ data, id }: CollectionEntry<"blog">) => {
   const isPublishTimePassed =
     Date.now() >
     new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin;
-  return !data.draft && (import.meta.env.DEV || isPublishTimePassed);
+
+  // 屏蔽原主题的示例文章
+  const isNotRelease = !id.startsWith("_releases/");
+
+  return !data.draft && (import.meta.env.DEV || isPublishTimePassed) && isNotRelease;
 };
 
 export default postFilter;
