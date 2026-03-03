4417ed050c9a6ccbc0857aaf556de5d7bb97d746
jxgla <hjxsdfz@gmail.com>
2026-03-03 11:40:22 +0000

    fix: 修复英文博文页面 404 问题，补全英文文章动态路由

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -21,7 +21,8 @@ export function getPath(
     .slice(0, -1) // remove the last segment_ file name_ since it's unnecessary
     .map(segment => slugifyStr(segment)); // slugify each segment path
 
-  const basePath = includeBase ? "/posts" : "";
+  const isEn = id.endsWith("-en");
+  const basePath = includeBase ? (isEn ? "/en/posts" : "/posts") : "";
 
   // Making sure `id` does not contain the directory
   const blogId = id.split("/");
